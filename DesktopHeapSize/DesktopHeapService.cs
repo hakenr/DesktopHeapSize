f60821e9d15bda2808c2f498a861310c68531e07
Robert Haken <haken@havit.cz>
2015-06-10 13:37:25 +0200

    Initial revision

---
+++ DesktopHeapSize/DesktopHeapService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Havit.DesktopHeapSize
+{
+	public class DesktopHeapService
+	{
+		public ulong GetCurrentDesktopHeapSize()
+		{
+#pragma warning disable CS0618 // Type or member is obsolete
+			IntPtr desktopHandle = GetThreadDesktop((uint)AppDomain.GetCurrentThreadId());
+#pragma warning restore CS0618 // Type or member is obsolete
+
+			uint nLength = 4;
+            byte[] result = new byte[nLength];
+			uint lpnLengthNeeded;
+            GetUserObjectInformation(desktopHandle, UOI_HEAPSIZE, result, nLength, out lpnLengthNeeded);
+
+			return BitConverter.ToUInt32(result, 0);
+		}
+
+
+		/// <remarks>
+		/// https://msdn.microsoft.com/en-us/library/windows/desktop/ms683232(v=vs.85).aspx
+		/// http://www.pinvoke.net/default.aspx/user32.getthreaddesktop
+		/// </remarks>
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern IntPtr GetThreadDesktop(uint dwThreadId);
+
+		/// <remarks>
+		/// https://msdn.microsoft.com/en-us/library/ms683238.aspx
+		/// http://www.pinvoke.net/default.aspx/user32.getuserobjectinformation
+		/// </remarks>
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern bool GetUserObjectInformation(IntPtr hObj, int nIndex, [Out] byte[] heapSize, uint nLength, out uint lpnLengthNeeded);
+
+		private const int UOI_FLAGS = 1;
+		private const int UOI_NAME = 2;
+		private const int UOI_TYPE = 3;
+		private const int UOI_USER_SID = 4;
+		private const int UOI_HEAPSIZE = 5; //Windows Server 2003 and Windows XP/2000:  This value is not supported.
+		private const int UOI_IO = 6;       //Windows Server 2003 and Windows XP/2000:  This value is not supported.
+
+
+	}
+}
---
+++ Tests/DesktopHeapServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Havit.DesktopHeapSize;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class DesktopHeapServiceTest
+	{
+		[TestMethod]
+		public void DesktopHeapService_GetCurrentDesktopHeapSize_ShouldReturnPositiveNumber()
+		{
+			DesktopHeapService service = new DesktopHeapService();
+
+			ulong result = service.GetCurrentDesktopHeapSize();
+
+			Assert.IsTrue(result > 0);
+		}
+	}
+}
