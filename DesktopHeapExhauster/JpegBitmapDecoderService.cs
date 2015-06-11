1b163a961d60e68b77e88117746ae483e26c3f42
Robert Haken <haken@havit.cz>
2015-06-11 17:10:51 +0200

    DesktopHeapExhauster

---
+++ DesktopHeapExhauster/JpegBitmapDecoderService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace DesktopHeapExhauster
+{
+	public class JpegBitmapDecoderService
+	{
+		public void ReadExifDataUsingJPEGDecoder(string filename)
+		{
+			using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+			{
+				BitmapDecoder decoder = new JpegBitmapDecoder(fs, BitmapCreateOptions.None, BitmapCacheOption.None);
+				if (decoder != null && decoder.Frames != null)
+				{
+					foreach (var frm in decoder.Frames)
+					{
+						if (frm != null && frm.Metadata != null)
+						{
+							var meta = frm.Metadata as BitmapMetadata;
+							if (meta != null)
+							{
+								if (!String.IsNullOrWhiteSpace(meta.Title))
+								{
+									var title = meta.Title.Trim();
+								}
+								if (!String.IsNullOrWhiteSpace(meta.Comment))
+								{
+									var comment = meta.Comment.Trim();
+								}
+								DateTime taken;
+								if (!String.IsNullOrWhiteSpace(meta.DateTaken) && DateTime.TryParse(meta.DateTaken, out taken))
+								{
+									// NOOP
+								}
+							}
+
+						}
+					}
+				}
+				fs.Close(); //not needed if using? 
+			}
+		}
+	}
+}
---
+++ DesktopHeapExhauster/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+namespace DesktopHeapExhauster
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var decoderService = new JpegBitmapDecoderService();
+			int i = 0;
+			do
+			{
+				i++;
+				Thread t = new Thread(() => 
+				{
+					//Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+
+					decoderService.ReadExifDataUsingJPEGDecoder(@"D:\Temp\IMG_0235.JPG");
+
+					//dispatcher.InvokeShutdown();
+				});
+				t.Start();
+
+				Console.WriteLine(i);
+			}
+			while (true);
+		}
+	}
+}
